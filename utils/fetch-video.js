ab26828bcc9af19b084ceac8e5ac62d3001a8e80
yarndinasti <ian.ian.wold76@gmail.com>
2022-04-07 16:38:05 +0700

    fix time remaining

--- utils/fetch-video.js
+++ utils/fetch-video.js
@@ -123,7 +123,7 @@ async function fetchVideoData(notif = false) {
 
       switch (video_data.live_status) {
         case "upcoming":
-          const time_remain = moment(video_data.live.start_time).fromNow()
+          const time_remain = moment(video_data.live.start_time).toNow()
           console.log(
             chalk.blue.inverse.bold(` 🎬 Staring in ${time_remain} `) +
               video_data.title
